ba2528b585e1e6febc41fb91a6178ba17da99416
smnkv <andrii.semenkov@gmail.com>
2020-12-11 17:25:31 +0100

    4. Class: add interface and change the order of constructor params. Observe the power of named parameters

--- src/main/kotlin/com/kotlin/training/kt/trainings/Application.kt
+++ src/main/kotlin/com/kotlin/training/kt/trainings/Application.kt
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 class Application
 
 fun main(args: Array<String>) {
-    val kotlin = Language("Kotlin", "Good", 9)
-    val java = Language("Java", "Bad", 25)
+    val kotlin = Language(name = "Kotlin", description = "Good", age = 9)
+    val java = Language(name = "Java", description = "Bad", age = 25)
     val javaClone = java.copy(description = "Not so bad, but still shitty")
 
     println(kotlin)
@@ -18,10 +18,15 @@ fun main(args: Array<String>) {
 //    runApplication<Application>(*args)
 }
 
+interface ProgrammingLanguage {
+    val name: String
+    val description: String
+    val age: Byte
+}
 
-data class Language(private val name: String,
-                    private val description: String,
-                    private val age: Byte) {
+data class Language(override val name: String,
+                    override val age: Byte,
+                    override val description: String) : ProgrammingLanguage {
 
     override fun toString(): String {
         return "Hello I'm $name, I'm $age years old I'm $description programming language"
