7bd08f5062994d1db3dec514581f1c90f8bbcb50
smnkv <andrii.semenkov@gmail.com>
2020-12-11 17:18:53 +0100

    3. Class: data class taking care of equals and hashcode for us

--- src/main/kotlin/com/kotlin/training/kt/trainings/Application.kt
+++ src/main/kotlin/com/kotlin/training/kt/trainings/Application.kt
@@ -18,31 +18,11 @@ fun main(args: Array<String>) {
 }
 
 
-class Language(private val name: String,
-               private val description: String,
-               private val age: Byte) {
+data class Language(private val name: String,
+                    private val description: String,
+                    private val age: Byte) {
 
     override fun toString(): String {
         return "Hello I'm $name, I'm $age years old I'm $description programming language"
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Language
-
-        if (name != other.name) return false
-        if (description != other.description) return false
-        if (age != other.age) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = name.hashCode()
-        result = 31 * result + description.hashCode()
-        result = 31 * result + age
-        return result
     }
 }
