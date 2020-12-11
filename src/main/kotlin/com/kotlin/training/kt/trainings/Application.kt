f29c6559c14bacdabc2da38d00f26a1c3af2d748
smnkv <andrii.semenkov@gmail.com>
2020-12-11 17:17:51 +0100

    2. Class: how to compare objects in kotlin

--- src/main/kotlin/com/kotlin/training/kt/trainings/Application.kt
+++ src/main/kotlin/com/kotlin/training/kt/trainings/Application.kt
@@ -8,9 +8,12 @@ class Application
 fun main(args: Array<String>) {
     val kotlin = Language("Kotlin", "Good", 9)
     val java = Language("Java", "Bad", 25)
+    val javaClone = Language("Java", "Bad", 25)
 
     println(kotlin)
     println(java)
+    println(java == javaClone) //call Language.equals
+    println(java === javaClone) //call Object.equals
 //    runApplication<Application>(*args)
 }
 
